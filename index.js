33a3b146c8e73ef44e5508112cccf6b205d8dbe2
Mac Rowe <m.rowe42@gmail.com>
2021-06-03 10:50:10 -0400

    commit tree 52f6b0b92c10a703361ca6fceaa3ff6bf1cdb86a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(array, fn) {
+    return array.map(fn);
+}
+
+function reduce(array, fn, startingValue) {
+    if(startingValue) {
+        return array.reduce(fn) + startingValue;
+    } else {
+       return array.reduce(fn)
+    }
+}
